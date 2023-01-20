d459a91f9ab6aa5fd581462cba2b29c0f37bda2d
bcipa <39407286+bcipa@users.noreply.github.com>
2023-01-20 08:35:21 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import Pathfinder from './components/Pathfinder.vue';
+export default {
+  name: 'app',
+  components: {
+    Pathfinder,
+  },
+  data: function () {
+    return {
+      maze: [
+        [0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, 1, 0, 0],
+        [0, 1, 0, 1, 'x'],
+        [0, 1, 1, 1, 0],
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <Pathfinder v-bind:maze="maze" />
+</template>
+
+<style scoped></style>
---
+++ src/components/Pathfinder.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    maze: Array,
+  },
+  methods: {
+    perform() {
+      console.log('LETS GO');
+
+      console.log('DONE');
+    },
+  },
+});
+</script>
+
+<template>
+  <div v-for="(row, index) in maze" v-bind:key="index">
+    <span v-for="(value, column) in row" v-bind:key="column">{{ value }}</span>
+  </div>
+</template>
+
+<style scoped></style>
